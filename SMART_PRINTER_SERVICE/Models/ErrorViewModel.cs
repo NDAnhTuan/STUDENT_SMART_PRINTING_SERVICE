5184a9102251cb542e2884d7e397b04b3c9a90ff
Your Name <nguyenducanhtuan0602@gmail.com>
2023-11-02 13:43:47 +0700

    Add project files.

---
+++ SMART_PRINTER_SERVICE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SMART_PRINTER_SERVICE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
