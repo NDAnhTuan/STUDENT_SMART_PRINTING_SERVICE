722a8df0bf9d11a0c4b504738fbc9cb01c8afc0c
unknown <diem.nguyenhv23@hcmut.edu.vn>
2023-11-08 00:22:00 +0700

    them folder print va printController

---
+++ SMART_PRINTER_SERVICE/Controllers/PrintController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMART_PRINTER_SERVICE.Models;
+using System.Diagnostics;
+
+namespace SMART_PRINTER_SERVICE.Controllers
+{
+    public class PrintController : Controller
+    {
+
+        public IActionResult PrintPreview()
+        {
+            return View();
+        }
+    }
+}
